1dc1123ef8f67f6c51c875c8997df7eb3e9dd79f
Yakira <zebzeeff.kirill@yandex.ru>
2024-07-06 01:36:40 +0300

    появляется +- понимание архитектуры и команд

--- config_data/config.py
+++ config_data/config.py
@@ -10,7 +10,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 DB_NAME = os.getenv("DB_NAME")
 DEFAULT_COMMANDS = (('start', "Запуск бота"),
-                    ('help', 'Вывод команд бота (Справочник)'),
-                    ('history', 'Вывод истории запросов'),
-                    ('', ''),
+                    ('help', 'Получить справочник'),
+                    ('day_watch', 'Получить данные торгового дня'),
+                    ('equity', 'Получить рыночные данные'),
+                    ('symbol_data', 'Получить данные о компании'),
+                    ('news', 'Получение новостей по маркеру'),
+                    ('analysis', 'Посмотреть анализ по символу'),
                     )
