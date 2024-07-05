5d4594bcdc1c5f66dffd1e4c4896b02fc73bc777
Yakira <zebzeeff.kirill@yandex.ru>
2024-07-06 01:37:32 +0300

    мелочи

--- handlers/default_handlers/help.py
+++ handlers/default_handlers/help.py
@@ -1,10 +1,12 @@
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram import Router
+from config_data.config import DEFAULT_COMMANDS
 
 router_1 = Router()
 
 
 @router_1.message(Command('help'))
 async def cmd_help(message: Message):
-    await message.answer(f"Команда /help")
+    text = "\n".join([f'/{k} - {i}' for k, i in DEFAULT_COMMANDS])
+    await message.answer(f"Это бот помощник по финансовому рынку! Его команды:\n{text}")
--- loader.py
+++ loader.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher, Bot
 from aiogram.fsm.storage.memory import MemoryStorage
 from config_data import config
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
-bot = Bot(token=config.BOT_TOKEN)  # Объект бота
-storage = MemoryStorage()  # Объект сохранения пользовательских состояний
-dp = Dispatcher(storage=storage)
+bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+storage = MemoryStorage()
+dp = Dispatcher(bot=bot, storage=storage)
