0be008ca3a294886153bc24b986eaaa2aaeb78c7
Yakira <zebzeeff.kirill@yandex.ru>
2024-07-06 01:38:02 +0300

    добавление пользователя в бд

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,7 +1,8 @@
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 from loguru import logger
 from aiogram import Router
+from database.add_to_db import add_user
 
 router = Router()
 
@@ -10,4 +11,5 @@ logger.info("Команда start активирована")
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(f'Привет {message.chat.full_name}. Рад тебя видеть!')
+    await add_user(message)
+    await message.answer(f'Привет {message.from_user.full_name}. Рад тебя видеть!')
